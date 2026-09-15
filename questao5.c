9e158833b252ecef8842d67c14e6affbccb25aa4
jvqmg <jvqmg@cesar.school>
2026-09-14 21:41:59 -0300

    Delete questao5.c

--- questao5.c
+++
@@ -1,8 +0,0 @@
-//falta incluir as bibliotecas <stdio.h> para o printf e a <stdlib.h> para o system
-//além disso, a função main não está declarada como int e nem tem o return 0
-
-main()
-{
-printf("Linguagem C");
-system("pause");
-}
