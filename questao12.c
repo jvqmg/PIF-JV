10f46c7f02d6db39fd456e518f1d741f9f8c067a
jvqmg <jvqmg@cesar.school>
2026-09-14 21:38:38 -0300

    Delete questao12.c

--- questao12.c
+++
@@ -1,8 +0,0 @@
-a) int a; [correto] [declaracao válida]
-b) float b; [correto] [declaracao válida]
-c) double float c; [incorreto] [sintaxe inválida]
-d) unsigned char d; [correto] [declaracao válida]
-e) unsigned e; [correto] [sintaxe válida]
-f) long float f; [incorreto] [tipo inválido]
-g) long g; [correto] [declaracao válida]
-h) long double h; [correto] [declaracao válida]
